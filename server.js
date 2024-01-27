5ded607b1647afd6d9fee77b1c1c562bd54131e3
Nyakamba <omwegaenock@gmail.com>
2024-01-27 18:04:49 +0300

    comments routing

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const userRoutes = require("./routes/users/users");
 const postRoutes = require("./routes/posts/posts");
+const commentRoutes = require("./routes/comments/comments");
 require("./config/dbConnect");
 const app = express();
 
@@ -14,41 +15,7 @@ app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
 
 //comments route
-//POST/api/comments
-app.post("/api/comments", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "Comment ctreated" });
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-//GET/api/comments/:id
-app.get("/api/comments/:id", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "Comment details" });
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-//DELETE/api/comments/:id
-app.delete("/api/comments/:id", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "Comment deleted" });
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-//PUT/api/comments/:id
-app.put("/api/comments/:id", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "Commnet updated" });
-  } catch (error) {
-    res.json(error);
-  }
-});
+app.use("/api/comments", commentRoutes);
 
 //Error handler middlewares
 //liten server

3d11417021e840fa03c65037152794585568d945
Nyakamba <omwegaenock@gmail.com>
2024-01-27 16:43:53 +0300

    express routing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const userRoutes = require("./routes/users/users");
 require("./config/dbConnect");
 const app = express();
 
@@ -8,87 +9,7 @@ const app = express();
 //---------
 //users route
 //----------
-
-//POST/api/users/register
-app.post("/api/users/register", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "User registered" });
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-//POST/api/users/login
-app.post("/api/users/login", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "User login" });
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-//GET/api/user/:id
-app.get("/api/users/:id", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "User details" });
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-//GET/api/users/profile/:id
-app.get("/api/users/profile/:id", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "User profile" });
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-//PUT/api/users/profile-photo-upload/:id
-app.put("/api/users/profile-photo-upload/:id", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "User profile-photo upload" });
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-//PUT/api/users/cover-photo-upload/:id
-app.put("/api/users/cover-photo-upload/:id", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "User cover-photo upload" });
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-//PUT/api/users/update-password/:id
-app.put("/api/users/update-password/:id", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "User update password" });
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-//PUT/api/users/update/:id
-app.put("/api/users/update/:id", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "User updated" });
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-//GET/api/users/logout
-app.get("/api/users/logout", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "User logout" });
-  } catch (error) {
-    res.json(error);
-  }
-});
+app.use("/", userRoutes);
 
 //---------
 //posts route

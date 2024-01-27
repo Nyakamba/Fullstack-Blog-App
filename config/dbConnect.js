30c48b26e52e8348d45c71c7c33dc051b4c8c2ce
Nyakamba <omwegaenock@gmail.com>
2024-01-27 14:06:12 +0300

    connecting to the database

---
+++ config/dbConnect.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("DB Connected Successifully");
+  } catch (error) {
+    console.log("Db Connection Failed", error.message);
+  }
+};
+dbConnect();

1aca88a0f384df14f442ee7d557662f7728a227e
Nyakamba <omwegaenock@gmail.com>
2024-01-27 21:30:05 +0300

    global error handler

--- middlewares/globalErrHandler.js
+++ middlewares/globalErrHandler.js
@@ -1,1 +1,17 @@
-const globalErrHandler = () => {};
+const globalErrHandler = (err, req, res, next) => {
+  //status:failed/something/server error
+  //message
+  //stack
+  const stack = err.satck;
+  const message = err.message;
+  const status = err.status ? err.status : "failed";
+  const statusCode = err.statusCode ? err.statusCode : 500;
+
+  res.status(statusCode).json({
+    status,
+    message,
+    stack,
+  });
+};
+
+module.exports = globalErrHandler;

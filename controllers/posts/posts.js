8a57228b3934784b9717e1e03a759dd53c34569e
Nyakamba <omwegaenock@gmail.com>
2024-01-27 18:41:19 +0300

    posts controllers

---
+++ controllers/posts/posts.js
@@ -0,0 +1,49 @@
+//create post
+const createPostCtrl = async (req, res) => {
+  try {
+    res.json({ status: "success", user: "Post ctreated" });
+  } catch (error) {
+    res.json(error);
+  }
+};
+//get all posts
+const fetchPostsCtrl = async (req, res) => {
+  try {
+    res.json({ status: "success", user: "Post list" });
+  } catch (error) {
+    res.json(error);
+  }
+};
+//fetch a post
+const fecthPostCtrl = async (req, res) => {
+  try {
+    res.json({ status: "success", user: "Post details" });
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+//delete
+const deletePostCtrl = async (req, res) => {
+  try {
+    res.json({ status: "success", user: "Post deleted" });
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+//update
+const updatePostCtrl = async (req, res) => {
+  try {
+    res.json({ status: "success", user: "Post updated" });
+  } catch (error) {
+    res.json(error);
+  }
+};
+module.exports = {
+  createPostCtrl,
+  fetchPostsCtrl,
+  fecthPostCtrl,
+  deletePostCtrl,
+  updatePostCtrl,
+};

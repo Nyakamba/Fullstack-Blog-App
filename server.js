022e5dd710a3d535678945331b6ab780e397ab99
Nyakamba <omwegaenock@gmail.com>
2024-01-27 13:28:29 +0300

    installing packages

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+console.log(app);
+
+//middlewares
+//routes
+//Error handler middlewares
+//liten server
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, console.log(`Server is running on port${PORT}`));

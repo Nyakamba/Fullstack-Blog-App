1367bbf8643d44dcd912f69a18fabc1779291589
Nyakamba <omwegaenock@gmail.com>
2024-01-27 18:00:14 +0300

    posts routing

--- routes/posts/posts.js
+++ routes/posts/posts.js
@@ -1,3 +1,50 @@
 const express = require("express");
 
-const userRoutes = express.Router();
+const postRoutes = express.Router();
+
+//POST/api/v1/posts
+postRoutes.post("/", async (req, res) => {
+  try {
+    res.json({ status: "success", user: "Post ctreated" });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//GEt/api/v1/posts
+postRoutes.get("/", async (req, res) => {
+  try {
+    res.json({ status: "success", user: "Post list" });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//GET/api/v1/posts/:id
+postRoutes.get("/:id", async (req, res) => {
+  try {
+    res.json({ status: "success", user: "Post details" });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//DELETE/api/v1/posts/:id
+postRoutes.delete("/:id", async (req, res) => {
+  try {
+    res.json({ status: "success", user: "Post deleted" });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//PUT/api/v1/posts/:id
+postRoutes.put("/:id", async (req, res) => {
+  try {
+    res.json({ status: "success", user: "Post updated" });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+module.exports = postRoutes;

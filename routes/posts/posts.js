2698c571d4d8a36c1e8799ba2ffca536846db9be
Nyakamba <omwegaenock@gmail.com>
2024-01-27 18:41:23 +0300

    posts controllers

--- routes/posts/posts.js
+++ routes/posts/posts.js
@@ -1,50 +1,26 @@
 const express = require("express");
-
 const postRoutes = express.Router();
+const {
+  createPostCtrl,
+  fetchPostsCtrl,
+  fecthPostCtrl,
+  deletePostCtrl,
+  updatePostCtrl,
+} = require("../../controllers/posts/posts");
 
 //POST/api/v1/posts
-postRoutes.post("/", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "Post ctreated" });
-  } catch (error) {
-    res.json(error);
-  }
-});
+postRoutes.post("/", createPostCtrl);
 
 //GEt/api/v1/posts
-postRoutes.get("/", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "Post list" });
-  } catch (error) {
-    res.json(error);
-  }
-});
+postRoutes.get("/", fetchPostsCtrl);
 
 //GET/api/v1/posts/:id
-postRoutes.get("/:id", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "Post details" });
-  } catch (error) {
-    res.json(error);
-  }
-});
+postRoutes.get("/:id", fecthPostCtrl);
 
 //DELETE/api/v1/posts/:id
-postRoutes.delete("/:id", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "Post deleted" });
-  } catch (error) {
-    res.json(error);
-  }
-});
+postRoutes.delete("/:id", deletePostCtrl);
 
 //PUT/api/v1/posts/:id
-postRoutes.put("/:id", async (req, res) => {
-  try {
-    res.json({ status: "success", user: "Post updated" });
-  } catch (error) {
-    res.json(error);
-  }
-});
+postRoutes.put("/:id", updatePostCtrl);
 
 module.exports = postRoutes;

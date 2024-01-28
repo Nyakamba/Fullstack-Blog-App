ec4c3aa1c6b2b015998a5f8e7ba58e04707d35a5
Nyakamba <omwegaenock@gmail.com>
2024-01-28 09:03:11 +0300

    Catching errors

--- utils/appErr.js
+++ utils/appErr.js
@@ -1,1 +1,8 @@
-const appErrr = () => {};
+const appErr = (message, statusCode) => {
+  let error = new Error(message);
+  error.stack = error.stack;
+  error.statusCode = statusCode ? statusCode : 500;
+  return error;
+};
+
+module.exports = appErr;

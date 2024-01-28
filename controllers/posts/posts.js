bfeb951981de073b54dd540efd08912e7652217a
Nyakamba <omwegaenock@gmail.com>
2024-01-28 15:56:33 +0300

    creating a post

--- controllers/posts/posts.js
+++ controllers/posts/posts.js
@@ -1,32 +1,54 @@
 const Post = require("../../model/post/Post");
 const User = require("../../model/user/User");
+const appErr = require("../../utils/appErr");
 
 //create post
-const createPostCtrl = async (req, res) => {
-  const { title, description, category, image } = req.body;
+const createPostCtrl = async (req, res, next) => {
+  const { title, description, category } = req.body;
   try {
+    if (!title || !description || !category || !req.file) {
+      return next(appErr("All fields are required"));
+    }
     //find user
     const userId = req.session.userAuth;
-    console.log(userId);
-    res.json({ status: "success", user: "Post ctreated" });
+    const userFound = await User.findById(userId);
+    //create post
+    const postCreated = await Post.create({
+      title,
+      description,
+      category,
+      user: userFound._id,
+      image: req.file.path,
+    });
+    //push the post creted into the array of users posts
+    userFound.posts.push(postCreated._id);
+    //resave the userFound
+    await userFound.save();
+    res.json({ status: "success", data: postCreated });
   } catch (error) {
     res.json(error);
   }
 };
 //get all posts
-const fetchPostsCtrl = async (req, res) => {
+const fetchPostsCtrl = async (req, res, next) => {
   try {
-    res.json({ status: "success", user: "Post list" });
+    //fetch all posts
+    const allPosts = await Post.find();
+    res.json({ status: "success", data: allPosts });
   } catch (error) {
-    res.json(error);
+    return next(appErr(error.message));
   }
 };
 //fetch a post
-const fecthPostCtrl = async (req, res) => {
+const fecthPostCtrl = async (req, res, next) => {
   try {
-    res.json({ status: "success", user: "Post details" });
+    //get the id from params
+    const id = req.params.id;
+    //find the post
+    const post = await Post.findById(id);
+    res.json({ status: "success", data: post });
   } catch (error) {
-    res.json(error);
+    return next(appErr(error.message));
   }
 };
 

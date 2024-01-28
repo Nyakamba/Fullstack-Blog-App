4e4f3778a8c628e5534de5ce1f8928a9dea36c4f
Nyakamba <omwegaenock@gmail.com>
2024-01-28 11:51:29 +0300

    cloudinary configuration

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,2 +1,24 @@
+require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+//config cloudinary
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+//instance of cloudinary storage
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  allowedFormats: ["jpg", "jpeg", "png"],
+  params: {
+    folder: "blog-app-v1",
+    transformation: [{ width: 500, height: 500, crop: "limit" }],
+  },
+});
+
+module.exports = storage;

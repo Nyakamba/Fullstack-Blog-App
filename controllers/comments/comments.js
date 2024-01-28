7efec747fe70e30bef7077bb90ca65d5261d9281
Nyakamba <omwegaenock@gmail.com>
2024-01-28 17:55:12 +0300

    Creating comments

--- controllers/comments/comments.js
+++ controllers/comments/comments.js
@@ -1,10 +1,32 @@
 const Comment = require("../../model/comment/Comment");
+const User = require("../../model/user/User");
+const Post = require("../../model/post/Post");
 
 //create
 
-const createCommentCtrl = async (req, res) => {
+const createCommentCtrl = async (req, res, next) => {
+  const { message } = req.body;
   try {
-    res.json({ status: "success", user: "Comment ctreated" });
+    //find the post
+    const post = await Post.findById(req.params.id);
+    //create comment
+    const comment = await Comment.create({
+      user: req.session.userAuth,
+      message,
+    });
+    //push comment to post
+    post.comments.push(comment._id);
+    //find the user
+    const user = await User.findById(req.session.userAuth);
+    //push comment to user
+    user.comments.push(comment._id);
+
+    //disable validation
+    //save
+
+    await post.save({ validationBeforeSave: false });
+    await user.save({ validationBeforeSave: false });
+    res.json({ status: "success", data: comment });
   } catch (error) {
     res.json(error);
   }

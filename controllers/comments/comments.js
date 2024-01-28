d400031094e4b721ab5914d14a209c4896f11512
Nyakamba <omwegaenock@gmail.com>
2024-01-28 18:15:51 +0300

    deleting comment

--- controllers/comments/comments.js
+++ controllers/comments/comments.js
@@ -42,7 +42,20 @@ const commentDetailsCtrl = async (req, res) => {
 //delete
 const deleteCommentCtrl = async (req, res) => {
   try {
-    res.json({ status: "success", user: "Comment deleted" });
+    //get the id from params
+    const id = req.params.id;
+    //find the comment
+    const comment = await Comment.findById(id);
+    //check if the comment belongs to the user
+    if (comment.user.toString() !== req.session.userAuth.toString()) {
+      return next(appErr("You are not allowed to delete this comment", 403));
+    }
+    //delete the comment
+    await Comment.findByIdAndDelete(id);
+    res.json({
+      status: "success",
+      user: "Comment has been  deleted successifully",
+    });
   } catch (error) {
     res.json(error);
   }
@@ -51,7 +64,26 @@ const deleteCommentCtrl = async (req, res) => {
 //update
 const updateCommentCtrl = async (req, res) => {
   try {
-    res.json({ status: "success", user: "Commnet updated" });
+    //get the id from params
+    const id = req.params.id;
+    //find the post
+    const post = await Post.findById(id);
+    //check if the post belongs to the user
+    if (post.user.toString() !== req.session.userAuth.toString()) {
+      return next(appErr("You are not allowed to update this post", 403));
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      {
+        title,
+        description,
+        category,
+        image: req.file.path,
+      },
+      { new: true }
+    );
+    res.json({ status: "success", data: updatedPost });
   } catch (error) {
     res.json(error);
   }

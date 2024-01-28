c143cbb5902b2481b5751ad43b0c237216fe2873
Nyakamba <omwegaenock@gmail.com>
2024-01-28 15:56:58 +0300

    getting a single post

--- routes/posts/posts.js
+++ routes/posts/posts.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const multer = require("multer");
 const postRoutes = express.Router();
 const {
   createPostCtrl,
@@ -8,9 +9,15 @@ const {
   updatePostCtrl,
 } = require("../../controllers/posts/posts");
 const protected = require("../../middlewares/protected");
+const storage = require("../../config/cloudinary");
+
+//instance of multer
+const upload = multer({
+  storage,
+});
 
 //POST/api/v1/posts
-postRoutes.post("/", protected, createPostCtrl);
+postRoutes.post("/", protected, upload.single("file"), createPostCtrl);
 
 //GEt/api/v1/posts
 postRoutes.get("/", fetchPostsCtrl);

f3a7827860ff01c85d17e392c132fbeb888087ea
Nyakamba <omwegaenock@gmail.com>
2024-01-28 09:03:06 +0300

    Catching errors

--- middlewares/globalErrHandler.js
+++ middlewares/globalErrHandler.js
@@ -2,7 +2,7 @@ const globalErrHandler = (err, req, res, next) => {
   //status:failed/something/server error
   //message
   //stack
-  const stack = err.satck;
+  const stack = err.stack;
   const message = err.message;
   const status = err.status ? err.status : "failed";
   const statusCode = err.statusCode ? err.statusCode : 500;

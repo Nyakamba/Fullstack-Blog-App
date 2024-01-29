5188c7e19e3787fc533a6eefc6595b44f51e4465
Nyakamba <omwegaenock@gmail.com>
2024-01-29 17:24:29 +0300

    Displaying error

--- routes/users/users.js
+++ routes/users/users.js
@@ -28,7 +28,7 @@ userRoutes.get("/login", (req, res) => {
 });
 //register form
 userRoutes.get("/register", (req, res) => {
-  res.render("users/register");
+  res.render("users/register", { error: " " });
 });
 
 //profile form

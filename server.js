a0c9b828a74fd072396b71f2ec4e196eb53ee4ef
Nyakamba <omwegaenock@gmail.com>
2024-01-29 16:47:35 +0300

    register user from client side

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.static("public"));
 app.use(express.static(__dirname, +"/public"));
 
 app.use(express.json()); //pass incoming data
+app.use(express.urlencoded({ extended: true })); // form data
 //session configure
 app.use(
   session({

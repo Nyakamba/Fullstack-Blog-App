0ee41296d700776b3aaf2e64021a9ef0601e3901
Nyakamba <omwegaenock@gmail.com>
2024-01-30 12:24:01 +0300

    uploading cover photo

---
+++ middlewares/globalHandler.js
@@ -0,0 +1,17 @@
+const globalErrHandler = (err, req, res, next) => {
+  //status:failed/something/server error
+  //message
+  //stack
+  const stack = err.stack;
+  const message = err.message;
+  const status = err.status ? err.status : "failed";
+  const statusCode = err.statusCode ? err.statusCode : 500;
+
+  res.status(statusCode).json({
+    status,
+    message,
+    stack,
+  });
+};
+
+module.exports = globalErrHandler;

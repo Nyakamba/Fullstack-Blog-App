802f6fcbea7e89df397d5f45cfe93b0e5e99fd63
Nyakamba <omwegaenock@gmail.com>
2024-01-30 12:23:44 +0300

    uploading cover photo

--- server.js
+++ server.js
@@ -3,26 +3,27 @@ const express = require("express");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const methodOverride = require("method-override");
-const userRoutes = require("./routes/users/users");
+const globalErrHandler = require("./middlewares/globalHandler");
+const commentRoutes = require("./routes/comments/comment");
 const postRoutes = require("./routes/posts/posts");
-const commentRoutes = require("./routes/comments/comments");
-const globalErrHandler = require("./middlewares/globalErrHandler");
+const userRoutes = require("./routes/users/users");
+
 require("./config/dbConnect");
+
 const app = express();
 
 //middlewares
 //configure ejs
 app.set("view engine", "ejs");
-//serve statice files
-app.use(express.static("public"));
-app.use(express.static(__dirname, +"/public"));
+//serve static files
+app.use(express.static(__dirname + "/public"));
 
 app.use(express.json()); //pass incoming data
-app.use(express.urlencoded({ extended: true })); // form data
+app.use(express.urlencoded({ extended: true })); //pass form data
 
 //method override
 app.use(methodOverride("_method"));
-//session configure
+//session config
 app.use(
   session({
     secret: process.env.SESSION_KEY,
@@ -30,7 +31,7 @@ app.use(
     saveUninitialized: true,
     store: new MongoStore({
       mongoUrl: process.env.MONGO_URL,
-      ttl: 24 * 60 * 60,
+      ttl: 24 * 60 * 60, //1 day
     }),
   })
 );
@@ -45,7 +46,7 @@ app.use((req, res, next) => {
   next();
 });
 
-//render homepage
+//render home
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -56,12 +57,11 @@ app.use("/api/v1/users", userRoutes);
 //posts route
 app.use("/api/v1/posts", postRoutes);
 
-//comments route
+//comments
 app.use("/api/v1/comments", commentRoutes);
 
 //Error handler middlewares
 app.use(globalErrHandler);
-//liten server
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+//listen server
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));
